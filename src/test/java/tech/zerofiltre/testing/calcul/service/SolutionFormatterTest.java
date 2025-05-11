72658b4760784273578f311ea5a2b0cdeccd25d2
leon legrand <50806869+assougnon@users.noreply.github.com>
2025-05-11 08:27:14 +0000

    Update Webdriver dependency 2

--- src/test/java/tech/zerofiltre/testing/calcul/service/SolutionFormatterTest.java
+++ src/test/java/tech/zerofiltre/testing/calcul/service/SolutionFormatterTest.java
@@ -5,17 +5,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class SolutionFormatterTest {
+public class SolutionFormatterTest {
 
   private SolutionFormatter solutionFormatter;
 
   @BeforeEach
-  void initFormatter() {
+  public void initFormatter() {
     solutionFormatter = new SolutionFormatterImpl();
   }
 
   @Test
-  void format_shouldFormatAnyBigNumber() {
+  public void format_shouldFormatAnyBigNumber() {
     // GIVEN
     final int number = 1234567890;
 
@@ -23,7 +23,7 @@ class SolutionFormatterTest {
     final String result = solutionFormatter.format(number);
 
     // THEN
-    assertThat(result).isEqualTo("1 234 567 890");
+    assertThat(result).isEqualTo("1\u202F234\u202F567\u202F890");
   }
 
 }
